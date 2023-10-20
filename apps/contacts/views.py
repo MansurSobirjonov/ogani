a10ae0aa61f0a643ed64b712f9e7fd12dd3f421f
MansurSobirjonov <mansursobirjonov.me@gmail.com>
2023-10-20 20:28:27 +0500

    first commit

---
+++ apps/contacts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .forms import ContactForm
+from .models import Subscribe
+from apps.products.models import Category
+
+
+# Create your views here.
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    email = request.GET.get('email')
+    if email:
+        Subscribe.objects.create(email=email)
+    ctx = {
+        'form': form,
+    }
+    return render(request, 'contact.html', ctx)
